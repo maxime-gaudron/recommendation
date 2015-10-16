f0c5ff935162a7af2af5c8573718e67a587e5406
Maxime Gaudron <maxime.gaudron@rocket-internet.de>
2015-10-16 18:08:36 +0200

    Simple json API

---
+++ api/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/deckarep/golang-set"
+	"github.com/gorilla/mux"
+)
+
+var rules map[string][]rule
+
+func main() {
+	fmt.Printf("Parsing CSV\n")
+	data, err := parseCsv("./output.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rules = data
+
+	fmt.Printf("Loaded %d transactions\n", len(rules))
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/recommand/{set}", recommand)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func stringsToSet(strings []string) mapset.Set {
+	set := mapset.NewThreadUnsafeSet()
+	for _, s := range strings {
+		set.Add(s)
+	}
+
+	return set
+}
+
+func recommand(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	query := strings.Split(mux.Vars(r)["set"], ",")
+	sort.Strings(query)
+
+	data, ok := rules[strings.Join(query, "")]
+	if !ok {
+		data = []rule{}
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+	log.Printf("%s\t%s\t%s", time.Since(start), r.Method, r.RequestURI)
+}
+
+type rule struct {
+	Antecedents []string
+	Consequents []string
+	Confidence  float64
+	Support     float64
+	Lift        float64
+}
+
+func (w rule) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]interface{}{
+		"lift":            w.Lift,
+		"support":         w.Support,
+		"confidence":      w.Confidence,
+		"recommendations": w.Consequents,
+	})
+}
+
+func parseCsv(file string) (map[string][]rule, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	csvr := csv.NewReader(f)
+	csvr.FieldsPerRecord = 5
+
+	headers := true
+	rules := map[string][]rule{}
+	for {
+		row, err := csvr.Read()
+
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			return rules, err
+		}
+
+		if headers {
+			headers = false
+			continue
+		}
+
+		antecedents := strings.Split(row[0], ";")
+		consequents := strings.Split(row[1], ";")
+		sort.Strings(antecedents)
+		sort.Strings(consequents)
+
+		r := rule{
+			Lift:        mustParseFloat(row[2]),
+			Support:     mustParseFloat(row[3]),
+			Confidence:  mustParseFloat(row[4]),
+			Antecedents: antecedents,
+			Consequents: consequents,
+		}
+
+		key := strings.Join(antecedents, "")
+		fmt.Printf("%+v\n", key)
+		rules[key] = append(rules[key], r)
+	}
+}
+
+func mustParseFloat(value string) float64 {
+	i, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
